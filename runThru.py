df13fa250cbbfeef989134026f045bcc4610e414
mcg29 <35386477+mcg29@users.noreply.github.com>
2020-09-02 14:56:44 -0400

    Add files via upload

---
+++ runThru.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#Written by mcg29_ (c) 2019 
+
+import os
+
+def runThru(dir, list):
+	dirList = os.listdir(dir)
+	for item in dirList:
+		item = dir + "/" + item #Here is what fixed it 
+		#print item
+		list.append(item)
+		if (os.path.isdir(item)):
+			#print item + " is a directory. Running Through..."	
+			runThru(item, list)	
+	return list #Return list of all files
